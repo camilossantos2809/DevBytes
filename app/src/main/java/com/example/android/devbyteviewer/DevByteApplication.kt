78ff7771726a6321d86f0e452ae1b0a389829179
camilossantos2809 <camilossantos.2809@gmail.com>
2021-04-23 22:33:53 -0300

    set constraints in WorkManager

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,9 +17,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -48,7 +47,19 @@ class DevByteApplication : Application() {
      * Setup WorkManager background job to 'fetch' new network data daily
      */
     private fun setupRecurringWork() {
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        setRequiresDeviceIdle(true)
+                    }
+                }
+                .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(15, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .build()
         WorkManager.getInstance().enqueueUniquePeriodicWork(
                 RefreshDataWorker.WORK_NAME,
                 ExistingPeriodicWorkPolicy.KEEP,
@@ -56,7 +67,7 @@ class DevByteApplication : Application() {
         )
     }
 
-    private fun delayedInit(){
+    private fun delayedInit() {
         applicationScope.launch {
             Timber.plant(Timber.DebugTree())
             setupRecurringWork()
